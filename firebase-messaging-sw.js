de8aa58d8204e835345b0d45e32706f57d58efd3
AcertosOnline <acertosonlinejogodobicho@gmail.com>
2025-03-09 14:40:25 -0300

    Add files via upload

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,25 @@
+importScripts('https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/11.4.0/firebase-messaging.js');
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA1C3iYQe22zhTP5HVj19atOZLROtba3rw",
+    authDomain: "jogo-do-bicho-421ff.firebaseapp.com",
+    databaseURL: "https://jogo-do-bicho-421ff-default-rtdb.firebaseio.com",
+    projectId: "jogo-do-bicho-421ff",
+    storageBucket: "jogo-do-bicho-421ff.firebasestorage.app",
+    messagingSenderId: "1023919123583",
+    appId: "1:1023919123583:web:b6c561fb121fe54f9e234a",
+    measurementId: "G-BPH150V2SG"
+};
+
+firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(payload => {
+    console.log('[firebase-messaging-sw.js] Mensagem em background:', payload);
+    const notification = payload.notification;
+    self.registration.showNotification(notification.title, {
+        body: notification.body,
+        icon: '/path/to/icon.png' // Opcional
+    });
+});
